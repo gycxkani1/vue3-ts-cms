ae2212984741817993e197932cf9d3d4baa0f197
gycxkani1 <gycxkani@outlook.com>
2025-04-09 11:57:45 +0800

    chore: Add ElConfigProvider component for element-plus integration

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,19 @@
     <router-link to="/">Login</router-link> |
     <router-link to="/main">Main</router-link>
   </nav> -->
-  <div class="app">
-    <router-view></router-view>
-  </div>
-
+  <el-config-provider :locale="locale">
+    <div class="app">
+      <router-view></router-view>
+    </div>
+  </el-config-provider>
   <!-- <router-view /> -->
 </template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+import zhCn from "element-plus/lib/locale/lang/zh-cn";
+const locale = ref(zhCn);
+</script>
 
 <style lang="less">
 .app {
